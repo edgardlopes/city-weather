0af244e5fdb7cab95acbb3feadfbff2bf6b92038
Edgard Lopes <edgard-rodrigo@hotmail.com>
2019-05-05 21:25:48 -0300

    fix crossorigin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://crossorigin.me/http://api.openweathermap.org/data/2.5',
+  apiUrl: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5',
   appId: 'a5255bcfdde6cbc5c68e66eb1b45e582'
 };
